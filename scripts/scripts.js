0f855a45c8b7b20912e172db9b01c92a145eb0f0
Febby <gunawan.febby@gmail.com>
2020-10-19 17:22:31 +0700

    Setting up js folder
    - Testing the scrollintoview function

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+// var element = document.querySelector("#about-toba");
+
+// // smooth scroll to element and align it at the bottom
+
+
+// element.scrollIntoView({ behavior: 'smooth', block: 'end'});
